9044d05fd40db2bb347b73b53d01d1f09a0d34f4
James Stephen DV Mariano <james.stephen.dv14@gmail.com>
2016-06-06 17:25:39 +0800

    Call AsciiMatrix methods in main menu

--- AsciiMatrixOperations.java
+++ AsciiMatrixOperations.java
@@ -7,13 +7,58 @@ public class AsciiMatrixOperations {
   // Main
   // ****************************************
   public static void main(String args[]) {
-    AsciiMatrix m = initializeAsciiMatrix();
+    AsciiMatrix matrix = initializeAsciiMatrix();
+
     System.out.println();
-    System.out.println(m);
+    System.out.println(matrix);
 
+    int choice = 0;
+    do {
+      System.out.println();
+      System.out.println(getMenu());
 
+      choice = promptUserForPostiveInt("");
+
+      switch (choice) {
+        case 1: 
+          searchMatrix(matrix);
+          break;
+        case 2: 
+          editMatrixCell(matrix);
+          break;
+        case 3: 
+          System.out.println(matrix);
+          break;
+        case 4: 
+          matrix = initializeAsciiMatrix();
+          break; 
+      }
+    } while (choice != 5);
+
+    System.out.println("Goodbye.");
   }
+  // ****************************************
+  // Menu
+  // ****************************************
+  public static String getMenu() {
+    StringBuilder menu = new StringBuilder();
 
+    menu.append("Select an option:" + System.lineSeparator());
+    menu.append("1. Search" + System.lineSeparator());
+    menu.append("2. Edit" + System.lineSeparator());
+    menu.append("3. Print" + System.lineSeparator());
+    menu.append("4. Reset" + System.lineSeparator());
+    menu.append("5. Exit" + System.lineSeparator());
+
+    return menu.toString().trim();
+  }
+  // ****************************************
+  // Search matrix
+  // ****************************************
+  private static void searchMatrix(AsciiMatrix m) {
+    String query = promptUserForString("Enter the search query: ");
+    System.out.println(m.getSearchResults(query));
+  }
   // ****************************************
   // Initialize a matrix
   // ****************************************
@@ -23,8 +68,35 @@ public class AsciiMatrixOperations {
     return new AsciiMatrix(rows, cols);
   }
   // ****************************************
+  // Edit a cell
+  // ****************************************
+  private static void editMatrixCell(AsciiMatrix m) {
+    int row = promptUserForPostiveInt("Enter row to edit: ");
+    int col = promptUserForPostiveInt("Enter column to edit: ");
+    String newVal = promptUserForString("Enter the new value: ");
+
+    m.set(row, col, newVal);
+  }
+  // ****************************************
   // Prompts
   // ****************************************
+  private static String promptUserForString(String promptMsg) {
+    System.out.print(promptMsg);
+
+    String input;
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
+    try {
+      input = reader.readLine();
+    }
+    catch (IOException e) {
+      input = "";
+    }
+
+    return input;
+  }
+
   private static int promptUserForPostiveInt(String promptMsg) {
     System.out.print(promptMsg);
 
